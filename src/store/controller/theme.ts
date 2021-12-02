823e9a516a9f97354872e6ccda9ad529a886e6af
William Yallop <hello@williamyallop.com>
2021-12-02 18:02:13 +0000

    [store]: added new function to handle writing to a single file.

--- src/store/controller/theme.ts
+++ src/store/controller/theme.ts
@@ -5,6 +5,25 @@ const config = require(path.resolve("./cms.config.ts"));
 const themeDir = config.directories.theme;
 
 
+
+// ------------------------------------ ------------------------------------
+//  Write to file
+// ------------------------------------ ------------------------------------
+const writeSingleFile = async (target: string, type: 'json', data: any) => {
+    const directory = path.resolve(themeDir + target);
+    try {
+        if(type === 'json') {
+            let writeData = JSON.stringify(data);
+            fse.writeFileSync(directory, writeData);
+            return true
+        }
+    }
+    catch {
+        return false
+    }
+}
+
+
 // ------------------------------------ ------------------------------------
 //  Get single file content
 // ------------------------------------ ------------------------------------
@@ -28,5 +47,6 @@ const verifyFileExists = async (target: string) => {
 
 export {
     getSingleFileContent,
-    verifyFileExists
+    verifyFileExists,
+    writeSingleFile
 }
--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="./modules/store/index.d.ts" />
 /// <reference path="./modules/validator.d.ts" />
 /// <reference path="./modules/core.d.ts" />
+/// <reference path="./models/index.ts" />
