7d07b722c1701f4d10773350368db72a6011a955
William Yallop <hello@williamyallop.com>
2021-12-03 11:22:26 +0000

    [types]: updated types directory

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="./modules/store/index.d.ts" />
 /// <reference path="./modules/validator.d.ts" />
 /// <reference path="./modules/core.d.ts" />
-/// <reference path="./models/index.ts" />
+/// <reference path="./models/index.d.ts" />
--- src/types/modules/store/component.d.ts
+++ src/types/modules/store/component.d.ts
@@ -1,8 +1,18 @@
 // Key: stor_
 
-interface stor_comp_saveSingleData {
+// saveSingle()
+interface stor_comp_saveSingleInp {
     name: string
     description: string
     file_name: string
     image?: string
+}
+
+// updateSingle()
+interface stor_comp_updateSingleInp {
+    file_name?: string
+    name?: string
+    description?: string
+    preview_url?: string
+    fields?: Array<string> // will be changed 
 }
