e2f03fa5fa0abecbd923c74435d7670850d2c15c
William Yallop <hello@williamyallop.com>
2021-12-24 15:57:01 +0000

    [types]: updated page model type to reflect the data we want each page to contain.

--- types/models/index.d.ts
+++ types/models/index.d.ts
@@ -38,9 +38,24 @@ interface mod_pageModel {
         title: string
         description: string
     }
-    components: Array<mod_pageComponentModel>
+    components: Array<mod_pageModelComponent>
 }
-interface mod_pageComponentModel {
-    component_id: mod_componentModel["id"]
-    content_type_id: mod_contentTypesDatabaseModel["id"] // this stores the postreSQL row ID for the contnet type data.
+interface mod_pageModelComponent {
+    id: mod_componentModel["id"]
+    file_name: mod_componentModel["file_name"]
+    file_path: mod_componentModel["file_path"]
+    name: mod_componentModel["name"]
+    description: mod_componentModel["description"]
+    preview_url: mod_componentModel["preview_url"]
+    date_added: mod_componentModel["date_added"]
+    date_modified: mod_componentModel["date_modified"]
+    content_types: Array<mod_pageModelComponentContentType>
+}
+interface mod_pageModelComponentContentType {
+    id: mod_contentTypesDatabaseModel["id"]
+    config_id: mod_contentTypesConfigModel["id"]
+    name: mod_contentTypesConfigModel["name"]
+    type: mod_contentTypesConfigModel["type"]
+    config: mod_contentTypesConfigModel["config"]
+    data: mod_contentTypesDatabaseModel["data"]
 }
