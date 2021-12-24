40a1899c25acabe1d4a99b900718ec88901f2368
William Yallop <hello@williamyallop.com>
2021-12-24 19:51:12 +0000

    [content-type-controller]: added new controller for content type, also added entry point in index.ts

---
+++ src/controller/content_type.ts
@@ -0,0 +1,36 @@
+
+// ------------------------------------ ------------------------------------
+// save single component content type
+// ------------------------------------ ------------------------------------
+const saveSingle = async (id: string) => {
+    // Check if file ith component ID exists in theme/config/content_types and save a single component content type object to it
+    // Else create the file and save a single component content type object to it
+}
+
+// ------------------------------------ ------------------------------------
+// get all component content types data
+// ------------------------------------ ------------------------------------
+const getComponentContentTypes = async () => {
+
+}
+
+// ------------------------------------ ------------------------------------
+// delete single component content type
+// ------------------------------------ ------------------------------------
+const deleteSingle = async () => {
+
+}
+
+// ------------------------------------ ------------------------------------
+// update single component content type
+// ------------------------------------ ------------------------------------
+const updateSingle = async () => {
+
+}
+
+export {
+    saveSingle,
+    getComponentContentTypes,
+    deleteSingle,
+    updateSingle
+}
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as componentController from './controller/component';
 import * as postsController from './controller/posts';
 import * as themeController from './controller/theme';
 import * as distController from './controller/dist';
+import * as contentTypeController from './controller/content_type';
 
 export {
     validate,
@@ -11,5 +12,6 @@ export {
     postsController,
     themeController,
     distController,
+    contentTypeController,
     generator
 };
