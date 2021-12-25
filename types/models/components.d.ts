e9a471eaef52a8721798cb7e1de442b97fe3fba1
William Yallop <hello@williamyallop.com>
2021-12-25 18:06:21 +0000

    [types]: moved model types into individual files for clarity.

---
+++ types/models/components.d.ts
@@ -0,0 +1,13 @@
+// Key: mod_
+
+// Component Model
+interface mod_componentModel {
+    id: string
+    file_name: string
+    file_path: string
+    name: string
+    description: string
+    preview_url: string
+    date_added: string
+    date_modified: string
+}
